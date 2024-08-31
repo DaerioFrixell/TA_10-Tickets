ffc32b049753e5923140d59c0afb8f3bb78d44a2
denis <anywaymaybe1@gmail.com>
2024-08-31 14:32:03 +0400

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>loh</div>
+  <p>{{ count }}</p>
+  <SomeComp />
+  <SomeComp />
+</template>
+
+<script setup lang="ts">
+const count = ref(1); // ref is auto-imported
+</script>
---
+++ components/SomeComp.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>some comp</div>
+</template>
